b211b175f7dd996234de81ea3b12b7402ef76e47
Atul Bora <atulbora250@gmail.com>
2025-06-01 01:43:23 +0530

    login changes

--- middleware.js
+++ middleware.js
@@ -1,7 +1,15 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;//used to redirect to the same page after login
         req.flash("error", "You must be logged in to create a new listing");
         return res.redirect("/login");
+    }
+    next();
+}
+
+module.exports.savedRedirectUrl = (req, res, next) => {
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 }
